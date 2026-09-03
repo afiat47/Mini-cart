66207617bdae5b7981aabef3f3ef628591c8cf0b
afiat47 <afiatkhan01@gmail.com>
2026-09-03 12:29:54 +0600

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/minicart/MinicartApplication.java
@@ -0,0 +1,13 @@
+package com.example.minicart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinicartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinicartApplication.class, args);
+    }
+
+}
