2ba49d81017118a067b78f34345b0642e112bf01
afiat47 <afiatkhan01@gmail.com>
2026-09-03 18:46:11 +0600

    feat: configure PostgreSQL datasource and Hibernate

--- src/main/java/com/example/minicart/controller/ProductController.java
+++ src/main/java/com/example/minicart/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.example.minicart.controller;
 
+import com.example.minicart.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +10,14 @@ import org.springframework.ui.Model;
 @RequestMapping("/products")
 public class ProductController {
 
+    private final ProductService productService;
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
     @GetMapping
     public String products(Model model) {
         model.addAttribute("pageTitle", "Products");
+        model.addAttribute("products",productService.getAllProducts());
         return "products";
     }
 }
---
+++ src/main/java/com/example/minicart/service/ProductService.java
@@ -0,0 +1,19 @@
+package com.example.minicart.service;
+
+import com.example.minicart.entity.Product;
+import com.example.minicart.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+}
