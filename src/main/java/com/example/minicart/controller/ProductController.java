a358197127dc4e93e6d926d172df8c65e125dc67
afiat47 <afiatkhan01@gmail.com>
2026-09-04 15:46:34 +0600

    feat: update existing products using shared form

--- src/main/java/com/example/minicart/controller/ProductController.java
+++ src/main/java/com/example/minicart/controller/ProductController.java
@@ -3,10 +3,7 @@ package com.example.minicart.controller;
 import com.example.minicart.entity.Product;
 import com.example.minicart.service.ProductService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
 
 @Controller
@@ -14,9 +11,12 @@ import org.springframework.ui.Model;
 public class ProductController {
 
     private final ProductService productService;
+
     public ProductController(ProductService productService) {
+
         this.productService = productService;
     }
+
     @GetMapping
     public String products(Model model) {
         model.addAttribute("pageTitle", "Products");
@@ -34,5 +34,12 @@ public class ProductController {
     public String saveProduct(@ModelAttribute Product product) {
         productService.saveProduct(product);
         return "redirect:/products";
+    }
+
+    @GetMapping("edit/{id}")
+    public String showEditFrom(@PathVariable Long id, Model model) {
+        Product product = productService.getProductByID(id);
+        model.addAttribute("product",product);
+        return "product-form";
     }
 }
--- src/main/java/com/example/minicart/service/ProductService.java
+++ src/main/java/com/example/minicart/service/ProductService.java
@@ -11,14 +11,20 @@ public class ProductService {
     private final ProductRepository productRepository;
 
     public ProductService(ProductRepository productRepository) {
+
         this.productRepository = productRepository;
     }
 
     public List<Product> getAllProducts() {
+
         return productRepository.findAll();
     }
 
     public Product saveProduct(Product product) {
         return productRepository.save(product);
+    }
+
+    public Product getProductByID(Long id) {
+        return productRepository.findById(id).orElseThrow();
     }
 }
